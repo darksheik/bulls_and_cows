0fd17c21f1a9da5c9b15cf751a7807504f2df164
Don Pflaster <dpflaster@gmail.com>
2016-08-25 07:39:43 -0400

    Refactor main method to functions - left side effect

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,21 +3,35 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
 int main() {
-	// introduce the game
-	constexpr int WORD_LENGTH = 5;
+	PrintIntro();
+    GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	return 0;
+}
+
+// introduce the game
+void PrintIntro() {
+
+	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?" << endl;
-	
-	// get a guess from the player
+	return;
+}
+
+// get a guess from the player
+string GetGuessAndPrintBack() {
+
 	cout << "Your guess, please: ";
-	
+
 	string Guess = "";
 	getline(cin, Guess);
 
 	// repeat the guess back to them
 	cout << endl << "You guessed: " << Guess << endl;
-
-	return 0;
+	return Guess;
 }
