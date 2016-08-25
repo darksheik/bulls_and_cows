96efa5e865300031e5f30aa498b7728e5c1f1fcf
Don Pflaster <dpflaster@gmail.com>
2016-08-25 07:17:49 -0400

    Initial harvesting of user guess

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	// introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?" << endl;
 	
+	// get a guess from the player
+	cout << "Your guess, please: ";
+	
+	string Guess = "";
+	cin >> Guess;
+
+	// repeat the guess back to them
+	cout << endl << "You guessed " << Guess << endl;
+
 	return 0;
 }
